cf3ec57564d48daad8d1348d929f65a5d2f5d2cf
dwarkesh <dwarkeshsaxena.ntf@example.com>
2026-04-02 14:43:46 +0530

    fix frontend 9

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.1.40'],
-  async rewrites() {
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL?.replace('/api/v1', '') || 'https://executive-ledger.onrender.com/api/v1'}/:path*`,
-      },
-    ]
-  },
 }
 
 export default nextConfig
